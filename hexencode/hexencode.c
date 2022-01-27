658a9ee2ed63f271a2ed604a630b9a6f8ba7e8f3
Ivan Pizhenko <IvanPizhenko@users.noreply.github.com>
2022-01-27 23:57:25 +0200

    wip

---
+++ hexencode/hexencode.c
@@ -0,0 +1,18 @@
+// SPDX-LICENSE-IDENTIFIER: BSD-3-CLAUSE
+
+// CRT
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+// System
+#include <fcntl.h>
+#include <unistd.h>
+
+void printUsageAndExit() __attribute((noreturn));
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+  }
+}
